557c7ff529e0f368772b320d55c7094997da3f74
Maria Donet <maria.donet@gmail.com>
2018-01-20 19:43:35 +0100

    UseCase protocol

---
+++ RandomUsersApp/domain/UseCase.swift
@@ -0,0 +1,14 @@
+//
+//  UseCase.swift
+//  RandomUsersApp
+//
+//  Created by Maria Donet Climent on 20/1/18.
+//  Copyright © 2018 MariaDonet. All rights reserved.
+//
+
+import Foundation
+
+
+protocol UseCase {
+    func execute()
+}
