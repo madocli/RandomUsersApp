30529ac085428cc469dd998b3cf289ac792113d4
Maria Donet <maria.donet@gmail.com>
2018-01-20 19:24:07 +0100

    Domain Entity

---
+++ RandomUsersApp/domain/entities/User.swift
@@ -0,0 +1,34 @@
+//
+//  User.swift
+//  RandomUsersApp
+//
+//  Created by Maria Donet Climent on 20/1/18.
+//  Copyright © 2018 MariaDonet. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    let userName: String
+    let email: String
+    let cell: String
+    let pictureLarge: String
+    let pictureThumbnail: String
+    
+    init(userName: String, email: String, cell: String, pictureL: String, pictureT: String) {
+        self.userName = userName
+        self.email = email
+        self.cell = cell
+        self.pictureLarge = pictureL
+        self.pictureThumbnail = pictureT
+    }
+    
+    init(randomUser: RandomUser) {
+        self.userName = randomUser.fullName
+        self.email = randomUser.email
+        self.cell = randomUser.cell
+        self.pictureLarge = randomUser.picture.large
+        self.pictureThumbnail = randomUser.picture.thumbnail
+    }
+    
+}
